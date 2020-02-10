01ef745b425a28de28f50a5f28e47571f532682c
Jenny Song <jenny.jieun.song@gmail.com>
2020-02-09 20:16:26 -0800

    added everyone

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -3,8 +3,11 @@ import 'package:flutter/material.dart';
 class AboutPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: AboutContainer()
+    return Material(
+      color: Colors.black,
+      child: Center(
+        child: AboutContainer()
+      )
     );
   }
 }
@@ -15,11 +18,79 @@ class AboutContainer extends StatefulWidget {
 }  
 
 class AboutContainerState extends State<AboutContainer> {
-  bool openEye = true;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('About')
+    return Column(
+      children: [
+        Expanded(
+          flex:2,
+          child: Container()
+        ),
+        Expanded(
+          flex:1,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                'Team Blink',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  decoration: TextDecoration.underline
+                ),
+              ),
+              Text(
+                'Inhye Choi',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'Jenny Song',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'Jimmy Kim',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'Shawn Jung',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                ),
+              )
+            ]
+          )
+        ),
+        Expanded(
+          flex:2,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 205.0),
+            // top: 0,
+            child: GestureDetector(
+              onTap: () {
+                Navigator.of(context).pushReplacementNamed('/titlePage');
+              },
+              child: Text(
+                'BACK',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                  color: Colors.white
+                )
+              )
+            )
+          )
+        )
+      ]
     );
   }
 }
